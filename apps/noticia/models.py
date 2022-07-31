4cfd2ca738f83c350edf6b193d442908e20e2546
FedericoMeza <federicojlmeza@outlook.com>
2022-07-31 01:51:55 -0300

    Html/Css

--- apps/noticia/models.py
+++ apps/noticia/models.py
@@ -1,22 +1,25 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
+# Create your models here.
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=250, null=False)
+    nombre = models.CharField(max_length=100, null = False, blank = False)
 
     def __str__(self):
         return self.nombre
 
-# Create your models here.
-
 class Noticia(models.Model):
-    titulo = models.CharField(max_length=250, null=False)
-    fecha = models.DateTimeField(auto_now_add=True)
-    texto = models.TextField(null=True)
-    activo = models.BooleanField(default=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
-    imagen = models.ImageField(upload_to='noticia', default='noticia/default.png')
-
+    titulo = models.CharField(max_length = 250, null = False)
+    fecha = models.DateTimeField(auto_now_add = True)
+    texto = RichTextField(null= True)
+    activo = models.BooleanField(default = True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL, null = True)
+    destacada = models.BooleanField(default = False)
+    imagen = models.ImageField(upload_to = 'noticia', default = 'noticia/default.png')
 
     def __str__(self):
         return self.titulo
+
+    def __unicode__(self):
+        return self.categoria
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -4,4 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Usuario(AbstractUser):
-    imagen = models.ImageField(upload_to='usuario', default='usuario/user-default.png')
+    imagen = models.ImageField(upload_to='usuario', default='usuario/default_user.png' )
+    email = models.EmailField(null = False, blank = False)
+    facebook = models.URLField(null = True, blank = True)
+    twitter = models.URLField(null = True, blank = True)
+    instagram = models.URLField(null = True, blank = True)
